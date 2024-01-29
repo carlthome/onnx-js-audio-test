85fa6c56a9e23ab0a2c70ea48da37d9e63bc34e0
Carl Thomé <carlthome@gmail.com>
2024-01-29 19:42:27 +0100

    Add initial model predict call

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
 import * as ort from "onnxruntime-web";
 
-console.log(ort);
+async function main() {
+  try {
+    const session = await ort.InferenceSession.create("./model.onnx");
+
+    const data = Float32Array.from([1.0, 2.0, 3.0]);
+    const dims = [3, 1];
+    const feeds = {
+      input: new ort.Tensor("float32", data, dims),
+    };
+    const fetches = await session.run(feeds);
+    const output = fetches.output.data;
+    console.log(output);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main();
